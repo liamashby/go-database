59a05bf114655db6da6107457581ad300933765d
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-07-21 17:05:46 +1000

    basic write to file

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("creating directory if it doesn't exist")
+
+	err := os.Mkdir("./data", 0750)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("creating a new file")
+	file, err := os.Create("./data/text.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	file.WriteString("test")
 }
