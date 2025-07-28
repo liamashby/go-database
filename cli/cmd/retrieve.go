ffcd9dbba5b189ac2fe196d82939b32b2fa15e7d
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-07-28 17:31:23 +1000

    basic retrieve function

--- cli/cmd/retrieve.go
+++ cli/cmd/retrieve.go
@@ -1,1 +1,17 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+)
+
+func retrieve(file_name string) error {
+	data, err := os.ReadFile(file_name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data)
+	return nil
+}
