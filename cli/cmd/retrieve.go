6e8dbfdbeec7a841570fd8071c7adc6d79cc7e14
Liam Ashby <liam@Liams-MacBook-Air.local>
2026-02-04 14:14:51 +1100

    added files to read

--- cli/cmd/retrieve.go
+++ cli/cmd/retrieve.go
@@ -10,39 +10,43 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var retrieveCmd = &cobra.Command{
-	Use:   "retrieve",
-	Short: "retrieve data",
-	Long:  `retrieve all of the data that was written to disk`,
-	Run: func(cmd *cobra.Command, args []string) {
-		read_index()
-	},
-}
-
 type Mapping struct {
 	ID   string `json:"id"`
 	File string `json:"file"`
 }
 
-func read_index() {
+func read_and_filter_index(args []string) []string {
 	jsonFile, err := os.Open("./data/index.json")
+
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	defer jsonFile.Close()
+
 	byteValue, _ := io.ReadAll(jsonFile)
 	var mappings []Mapping
 	json.Unmarshal(byteValue, &mappings)
-}
 
-func filter_index(mappings []Mapping, indicies []string) []Mapping {
-	var filtered_index []Mapping
+	var files_to_read []string
 	for i := 0; i < len(mappings); i++ {
-		if slices.Contains(indicies, mappings[i].ID) {
-			filtered_index = append(filtered_index, mappings[i])
+		if slices.Contains(args, mappings[i].ID) {
+			files_to_read = append(files_to_read, mappings[i].File)
 		}
 	}
-	return filtered_index
+	return files_to_read
+}
+
+func read_data_on_disk(files []string) {
+}
+
+var retrieveCmd = &cobra.Command{
+	Use:   "retrieve",
+	Short: "retrieve data",
+	Long:  `retrieve all of the data that was written to disk`,
+	Run: func(cmd *cobra.Command, args []string) {
+		read_and_filter_index(args)
+	},
 }
 
 func init() {
